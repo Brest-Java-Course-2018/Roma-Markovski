4fc769265b0ad66ac236c850af9dc3473b37bd4b
romark99 <romark@tut.by>
2018-02-14 15:25:26 +0300

    My first simple test.

--- src/main/java/com/epam/brest/course/App.java
+++ src/main/java/com/epam/brest/course/App.java
@@ -2,12 +2,9 @@ package com.epam.brest.course;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
     }
 }
