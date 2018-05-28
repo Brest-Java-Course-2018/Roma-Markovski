5b1afef34ced0c97a6af3ec5872b00f6e2695aaa
romark <romark@tut.by>
2018-05-29 01:08:34 +0300

    Added thymeleaf to all templates.

--- web-app/src/main/java/com/epam/brest/course/web_app/controllers/PublicationController.java
+++ web-app/src/main/java/com/epam/brest/course/web_app/controllers/PublicationController.java
@@ -1,20 +1,80 @@
 package com.epam.brest.course.web_app.controllers;
 
+import com.epam.brest.course.model.Publication;
+import com.epam.brest.course.model.Writer;
+import com.epam.brest.course.service.PublicationService;
+import com.epam.brest.course.service.WriterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
 
 @Controller
 public class PublicationController {
 
+    @Autowired
+    PublicationService publicationService;
+
+    @Autowired
+    WriterService writerService;
 
     @GetMapping(value = "/publications")
-    public String writers(Model model) {
+    public String getPublications(Model model) {
+        Collection<Publication> publications = publicationService.getPublications();
+        model.addAttribute("publications", publications);
         return "publications";
     }
 
     @GetMapping(value = "/publication")
-    public String writer(Model model) {
+    public String gotoAddPublication(Model model) {
+        Publication publication = new Publication();
+        Collection<Writer> writers = writerService.getWriters();
+        model.addAttribute("publication", publication);
+        model.addAttribute("writers", writers);
+        model.addAttribute("isNew", true);
         return "publication";
+    }
+
+    @PostMapping(value = "/publication")
+    public String addPublication(Publication publication, BindingResult result) {
+        if (result.hasErrors()) {
+            return "publication";
+        }
+        else {
+            this.publicationService.addPublication(publication);
+            return "redirect:/publications";
+        }
+    }
+
+    @GetMapping(value = "/publication/{id}")
+    public String gotoEditPublication(@PathVariable Integer id, Model model) {
+        Publication publication = publicationService.getPublicationById(id);
+        Collection<Writer> writers = writerService.getWriters();
+        model.addAttribute("publication", publication);
+        model.addAttribute("writers", writers);
+        model.addAttribute("isNew", false);
+        return "publication";
+    }
+
+    @PostMapping(value = "/publication/{id}")
+    public String editPublication(Publication publication, BindingResult result) {
+        if (result.hasErrors()) {
+            return "publication";
+        }
+        else {
+            this.publicationService.updatePublication(publication);
+            return "redirect:/publications";
+        }
+    }
+
+    @GetMapping(value = "/publication/{id}/delete")
+    public String deletePublicationById(@PathVariable Integer id, Model model) {
+        publicationService.deletePublicationById(id);
+        return "redirect:/publications";
     }
 }
--- web-app/src/main/java/com/epam/brest/course/web_app/controllers/WriterController.java
+++ web-app/src/main/java/com/epam/brest/course/web_app/controllers/WriterController.java
@@ -6,8 +6,10 @@ import com.epam.brest.course.service.WriterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -24,10 +26,47 @@ public class WriterController {
         return "writers";
     }
 
+    @GetMapping(value = "/writer")
+    public String gotoAddWriter(Model model) {
+        Writer writer = new Writer();
+        model.addAttribute("writer", writer);
+        model.addAttribute("isNew", true);
+        return "writer";
+    }
+
+    @PostMapping(value = "/writer")
+    public String addWriter(Writer writer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "writer";
+        }
+        else {
+            this.writerService.addWriter(writer);
+            return "redirect:/writers";
+        }
+    }
+
     @GetMapping(value = "/writer/{id}")
-    public String getWriterById(@PathVariable Integer id, Model model) {
+    public String gotoEditWriter(@PathVariable Integer id, Model model) {
         Writer writer = writerService.getWriterById(id);
         model.addAttribute("writer", writer);
+        model.addAttribute("isNew", false);
         return "writer";
+    }
+
+    @PostMapping(value = "/writer/{id}")
+    public String editWriter(Writer writer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "writer";
+        }
+        else {
+            this.writerService.updateWriter(writer);
+            return "redirect:/writers";
+        }
+    }
+
+    @GetMapping(value = "/writer/{id}/delete")
+    public String deleteWriterById(@PathVariable Integer id, Model model) {
+        writerService.deleteWriterById(id);
+        return "redirect:/writers";
     }
 }
